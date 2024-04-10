3d8104a8051a103f79f0b654bc726d54116e8bc2
DragonZ <pichitchai7679@gmail.com>
2024-04-10 22:11:47 +0700

    modified:   command.js

--- command.js
+++ command.js
@@ -1,63 +1,32 @@
 function handleKeyPress(event) {
-  if (event.key === "Enter") {
-    search();
-  }
+    if (event.key === 'Enter') {
+        search();
+    }
 }
-document.getElementById("searchButton").addEventListener("click", function () {
-  search();
+document.getElementById('searchButton').addEventListener('click', function() {
+    search();
 });
 function search() {
-  var searchText = document.getElementById("searchInput").value;
-  var namesPlants = [
-    "Maiasaura",
-    "Saichania",
-    "Diplodocus",
-    "Saltasaurus",
-    "Triceratops",
-    "Gallimimus",
-    "Iguanodon",
-    "Parasaurolophus",
-    "Stegosaurus",
-    "Apatosaurus",
-  ];
-  var namesmeats = [
-    "Compsognathus",
-    "Carcharodontosarus",
-    "Carnotaurus",
-    "Giganotosaurus",
-    "Coelophysis",
-    "Ceratosaurus",
-    "Deinonychus",
-    "Dilophosaurus",
-    "Tarbosaurus",
-    "Troodon",
-    "Tyrannosaurus",
-    "Baryonyx",
-    "velociraptor",
-    "Spinosaurus",
-    "Allosaurus",
-    "Oviraptor",
-    "Herrerasaurus",
-  ];
-
+  var searchText = document.getElementById('searchInput').value;
+  var namesPlants =  ['Maiasaura', 'Saichania', 'Diplodocus', 'Saltasaurus', 'Triceratops', 'Gallimimus', 'Iguanodon', 
+       'Parasaurolophus', 'Stegosaurus', 'Apatosaurus'];
+       var namesmeats =  ['Compsognathus', 'Carcharodontosarus', 'Carnotaurus', 'Giganotosaurus', 'Coelophysis', 'Ceratosaurus', 'Deinonychus', 
+       'Dilophosaurus', 'Tarbosaurus', 'Troodon', 'Tyrannosaurus', 'Baryonyx', 'velociraptor', 'Spinosaurus', 'Allosaurus', 'Oviraptor', 'Herrerasaurus'];
+      
   for (var i = 0; i < namesPlants.length; i++) {
-    var name = namesPlants[i];
-    if (name === searchText) {
-      window.location.replace(
-        "https://maidragonz.github.io/maprang/" + searchText + ".html"
-     
-      );
-      return; // ออกจากฟังก์ชันทันทีหลังจากที่ได้ทำการเปลี่ยน URL แล้ว
-    }
-  }
-  for (var i = 0; i < namesmeats.length; i++) {
+      var name = namesPlants[i];
+      if (name === searchText) {
+        window.location.replace('https://maidragonz.github.io/maprang/' + searchText +'.html');
+          return; // ออกจากฟังก์ชันทันทีหลังจากที่ได้ทำการเปลี่ยน URL แล้ว
+      }
+    } 
+    for (var i = 0; i < namesmeats.length; i++) {
     var name = namesmeats[i];
-    if (name === searchText) {
-      window.location.replace(
-        "https://maidragonz.github.io/vieew/" + searchText + ".html"
-     
-      );
-      return; // ออกจากฟังก์ชันทันทีหลังจากที่ได้ทำการเปลี่ยน URL แล้ว
+      if (name === searchText) {
+        
+        window.location.replace('https://maidragonz.github.io/vieew/' + searchText +'.html');
+          return; // ออกจากฟังก์ชันทันทีหลังจากที่ได้ทำการเปลี่ยน URL แล้ว
+      }
     }
   }
-}
+
