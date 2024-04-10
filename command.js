07d5785a55b4c19477a2d4070e99e12091522a63
DragonZ <pichitchai7679@gmail.com>
2024-04-10 15:51:05 +0700

    modified:   System.css 	modified:   command.js 	new file:   dinosor/img/Dilophosaurus.jpg 	new file:   dinosor/img/Stegosaurus.jpg 	new file:   dinosor/img/Triceratops.jpg 	modified:   index.html

--- command.js
+++ command.js
@@ -8,15 +8,25 @@ document.getElementById('searchButton').addEventListener('click', function() {
 });
 function search() {
   var searchText = document.getElementById('searchInput').value;
-  var names =  ['Compsognathus', 'Carcharodontosaurus', 'Carnotaurus', 'Giganotosaurus', 'Coelophysis', 'Ceratosaurus', 'Deinonychus', 
-       'Dilophosaurus', 'Tarbosaurus', 'Troodon', 'Tyrannosaurus', 'Baryonyx', 'Velociraptor', 'Spinosaurus', 'Allosaurus', 'Herrerasaurus', 
-       'Maiasaura', 'Saichania', 'Diplodocus', 'Saltasaurus', 'Triceratops', 'Gallimimus', 'Iguanodon', 'Diplodocus', 'Stegosaurus', 
-       'Apatosaurus'];
-  for (var i = 0; i < names.length; i++) {
-      var name = names[i];
+  var namesPlants =  ['Maiasaura', 'Saichania', 'Diplodocus', 'Saltasaurus', 'Triceratops', 'Gallimimus', 'Iguanodon', 
+       'Parasaurolophus', 'Stegosaurus', 'Apatosaurus'];
+       var namesmeats =  ['Compsognathus', 'Carcharodontosarus', 'Carnotaurus', 'Giganotosaurus', 'Coelophysis', 'Ceratosaurus', 'Deinonychus', 
+       'Dilophosaurus', 'Tarbosaurus', 'Troodon', 'Tyrannosaurus', 'Baryonyx', 'velociraptor', 'Spinosaurus', 'Allosaurus', 'Oviraptor', 'Herrerasaurus'];
+      
+  for (var i = 0; i < namesPlants.length; i++) {
+      var name = namesPlants[i];
       if (name === searchText) {
-          window.open = "https://www.youtube.com/";
+        window.open('https://maidragonz.github.io/maprang/' + searchText +'.html', '_blank');
           return; // ออกจากฟังก์ชันทันทีหลังจากที่ได้ทำการเปลี่ยน URL แล้ว
       }
+    } 
+    for (var i = 0; i < namesmeats.length; i++) {
+    var name = namesmeats[i];
+      if (name === searchText) {
+        
+        window.open('https://maidragonz.github.io/vieew/' + searchText +'.html', '_blank');
+          return; // ออกจากฟังก์ชันทันทีหลังจากที่ได้ทำการเปลี่ยน URL แล้ว
+      }
+    }
   }
-}
+
