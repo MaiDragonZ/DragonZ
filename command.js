533cf28e2143d9135e136d110d0d460ea3bdb53d
DragonZ <pichitchai7679@gmail.com>
2024-04-10 21:25:22 +0700

    modified:   command.js

--- command.js
+++ command.js
@@ -16,8 +16,7 @@ function search() {
   for (var i = 0; i < namesPlants.length; i++) {
       var name = namesPlants[i];
       if (name === searchText) {
-        window.location.replace('https://maidragonz.github.io/maprang/' + searchText +'.html');
-
+        window.open('https://maidragonz.github.io/maprang/' + searchText +'.html', '_self');
           return; // ออกจากฟังก์ชันทันทีหลังจากที่ได้ทำการเปลี่ยน URL แล้ว
       }
     } 
@@ -25,8 +24,7 @@ function search() {
     var name = namesmeats[i];
       if (name === searchText) {
         
-        window.location.replace('https://maidragonz.github.io/vieew/' + searchText +'.html');
-
+        window.open('https://maidragonz.github.io/vieew/' + searchText +'.html', '_self');
           return; // ออกจากฟังก์ชันทันทีหลังจากที่ได้ทำการเปลี่ยน URL แล้ว
       }
     }
